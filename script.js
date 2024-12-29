920bfe099c61f45e23b07434dcbfe4a80319dee1
Viktoria Stolz <88290448+viktoriastolz@users.noreply.github.com>
2024-12-29 21:46:54 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const ladder = {
+  step: 0,
+  
+  up: function () {
+    this.step++;
+    return this; 
+  },
+  
+  down: function () {
+    this.step--;
+    return this; 
+  },
+  
+  showStep: function () {
+    console.log(this.step); 
+    return this; 
+  }
+};
+
+ladder.up().up().down().showStep(); // 1
+
+
+
+
+
+
+
+
